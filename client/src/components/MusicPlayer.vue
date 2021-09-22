3a66864346cd877963d26858e2c5414744863d3a
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-09-22 14:42:32 +0200

    Created MusicPlayer component wich uses "youtube player"
    Created MusicPlayer component wich uses "youtube player" to play videos and more
    
    Co-Authored-By: Tobias Östergren <45110274+volture1@users.noreply.github.com>

---
+++ client/src/components/MusicPlayer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="play('DXxeOvvNNwc')">The Black Page #1 on piano</button>
+    <button @click="play('CtkZxnkbjtI')">The Black Page #2 live band</button>
+    <button @click="pause()">Pause</button>
+    <button @click="playVid()">Play</button>'
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    play(id){
+      // calling global variable
+      window.player.loadVideoById(id)
+      window.player.playVideo()
+    },
+    pause(){
+      window.player.pauseVideo()
+    },
+     playVid(){
+      window.player.playVideo()
+    }
+  }
+}
+</script>
---
+++ client/src/yt.js
@@ -0,0 +1,24 @@
+// gets called automatically when YouTube player loads
+function onYouTubeIframeAPIReady() {
+  // using global variable
+  window.player = new YT.Player('yt-player', {
+    height: '390',
+    width: '640',
+    //height: '0',
+    //width: '0',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// this function triggers when we change song in player
+// can be used to update things, like counters
+function onPlayerStateChange(event) {
+  if (event.data != YT.PlayerState.PLAYING) return
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
