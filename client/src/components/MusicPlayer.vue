83d69141a7bc02dae91c9e70a7470951bb7c13ca
Tobias Ostergren <ostergrentobias@gmail.com>
2021-09-23 23:02:18 +0200

    added a mute/unmute button toggle
    Co-Authored-By: JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>

--- client/src/components/MusicPlayer.vue
+++ client/src/components/MusicPlayer.vue
@@ -1,33 +1,54 @@
 <template>
   <div>
     <button @click="pause()">Pause</button>
-    <button @click="playVid()">Play</button>'
+    <button @click="playVid()">Play</button>
+
+    <div v-if="show">
+      <button @click="volumeMute(), (show = false)">Mute</button>
+    </div>
+    <div v-if="show == false">
+      <button @click="volumeUnMute(), (show = true)">Unmute</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
   computed: {
-    songId(){
+    songId() {
       return this.$store.state.currentSong;
-    }
+    },
   },
   watch: {
-    songId(newId, oldId){
-      this.play(newId)
+    songId(newId, oldId) {
+      this.play(newId);
     },
   },
-  methods:{
-    play(id){
-      window.player.loadVideoById(id)
-      window.player.playVideo()
+  methods: {
+    play(id) {
+      window.player.loadVideoById(id);
+      window.player.playVideo();
     },
-    pause(){
-      window.player.pauseVideo()
+    pause() {
+      window.player.pauseVideo();
     },
-     playVid(){
-      window.player.playVideo()
-    }
-  }
-}
+    playVid() {
+      window.player.playVideo();
+    },
+    volumeMute() {
+      window.player.mute();
+    },
+    volumeUnMute() {
+      window.player.unMute();
+    },
+    muteToggle() {
+      this.muteToggle = !this.enable;
+    },
+  },
+};
 </script>
