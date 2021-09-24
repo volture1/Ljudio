2733f208c44180a86aeb1783a49c1d9bc47cf7c5
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-09-24 11:00:20 +0200

    merge fixes
    Co-Authored-By: Tobias Östergren <45110274+volture1@users.noreply.github.com>
    Co-Authored-By: yunyan2020 <59655244+yunyan2020@users.noreply.github.com>
    Co-Authored-By: ErminUz <26276586+ErminUz@users.noreply.github.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,11 @@
-/* import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-}) */
-
-export default {
+  plugins: [vue()],
   proxy: {
     "/rest": "http://localhost:3001",
     "/api": "http://localhost:3001", 
   }
-}
+}) 
