95d92b53ca8e401d5da554e499dc1119c31f34f6
Ermin <uzunic.e@gmail.com>
2021-09-28 15:48:59 +0200

    sidebar design & currentUser shown

---
+++ client/src/assets/icons/Genresicon.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg width="19" height="12" viewBox="0 0 19 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="1" cy="11" r="1" fill="#C4C4C4" fill-opacity="0.25"/>
+    <circle cx="1" cy="6" r="1" fill="#C4C4C4" fill-opacity="0.25"/>
+    <circle cx="1" cy="1" r="1" fill="#C4C4C4" fill-opacity="0.25"/>
+    <rect x="4" width="15" height="2" rx="1" fill="#C4C4C4" fill-opacity="0.25"/>
+    <rect x="4" y="10" width="15" height="2" rx="1" fill="#C4C4C4" fill-opacity="0.25"/>
+    <rect x="4" y="5" width="15" height="2" rx="1" fill="#C4C4C4" fill-opacity="0.25"/>
+  </svg>
+</template>
